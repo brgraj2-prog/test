a8e3f5eca70b3982cfea58ffec17d01dca98d33a
brgraj2-prog <brgraj2@gmail.com>
2026-07-06 15:19:05 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 700,
+    webPreferences: {
+      nodeIntegration: false
+    }
+  });
+
+  win.loadFile('index.html'); // change path if index.html is in a subfolder e.g. 'public/index.html'
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
